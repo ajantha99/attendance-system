77b1c7e1011d2f39ae70cff206b1f9116d3950e4
ajantha99 <85469715+ajantha99@users.noreply.github.com>
2024-06-01 14:04:09 +0530

    #2, Wrote jwt code and checked with postman

---
+++ Server/src/main/java/com/task/backend/Server/controllers/UserController.java
@@ -0,0 +1,64 @@
+package com.task.backend.Server.controllers;
+
+import com.task.backend.Server.Entity.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.http.HttpStatus;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import java.security.Key;
+
+@RestController
+public class UserController {
+
+    private Map<String, String> users = new HashMap<>();
+
+    public UserController() {
+        // In a real-world scenario, you would retrieve users from a database
+        users.put("Ajanthini", "password");
+        users.put("user2", "password2");
+    }
+
+    private Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+    private long expirationTimeMillis = 3600000; // 24 hours in milliseconds
+
+
+
+    @PostMapping("/login")
+    public ResponseEntity<Map<String, String>> login(@RequestBody User user) {
+        String username = user.getUsername();
+        String password = user.getPassword();
+
+        if (users.containsKey(username) && users.get(username).equals(password)) {
+            String token = generateToken(username);
+            Map<String, String> response = new HashMap<>();
+            response.put("token", token);
+            return ResponseEntity.ok(response);
+        } else {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+    }
+
+    private String generateToken(String username) {
+
+        Date now = new Date();
+        Date expirationDate = new Date(now.getTime() + expirationTimeMillis); // Set expiration time
+
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(now)
+                .setExpiration(expirationDate)
+                .signWith(key)
+                .compact();
+    }
+}
